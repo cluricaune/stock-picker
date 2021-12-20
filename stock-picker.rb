d1f6a7157f4337fabe03b827a3b6beefab87960c
cluricaune <dunn.jake90@gmail.com>
2021-12-20 16:31:01 -0500

    .

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,5 +1,9 @@
 daily_price = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(daily_price)
-  
+  prices_hash = Hash.new(0)
+  daily_price.map {|num| prices_hash[num] = index(num)}
+  return prices_hash
 end
+
+p stock_picker(daily_price)
