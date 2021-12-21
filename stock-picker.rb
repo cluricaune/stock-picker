5c24053e4f6d839fc53e2de5c7512c9348facad5
cluricaune <dunn.jake90@gmail.com>
2021-12-21 15:30:56 -0500

    .

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,9 +1,26 @@
-daily_price = [17,3,6,9,15,8,6,1,10]
+require "pp"
 
 def stock_picker(daily_price)
-  prices_hash = Hash.new(0)
-  daily_price.map {|num| prices_hash[num] = index(num)}
-  return prices_hash
+  record = 0
+  day_to_buy = nil
+  day_to_sell = nil
+  daily_price.each do |x|
+    daily_price.each do |y|
+      if daily_price.index(x) < daily_price.index(y)
+        profit = -x + y
+        if profit > record
+          day_to_buy = x
+          day_to_sell = y
+          record = profit
+        end
+      end
+    end
+  end
+  p [daily_price.index(day_to_buy), daily_price.index(day_to_sell)]
 end
 
-p stock_picker(daily_price)
+
+
+daily_price = [10,9,8,7,6,50,4,3,2,1]
+#[17,3,6,9,15,8,6,1,10]
+stock_picker(daily_price)
